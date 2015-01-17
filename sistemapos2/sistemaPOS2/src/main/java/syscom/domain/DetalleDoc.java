976b23807be5c94fbce8df997e874e9259db6d1b
mikefor42 <mikefor42@hotmail.com>
2015-01-17 15:04:18 -0500

    Actualizacion

---
+++ sistemapos2/sistemaPOS2/src/main/java/syscom/domain/DetalleDoc.java
@@ -0,0 +1,70 @@
+package syscom.domain;
+
+public class DetalleDoc {
+	long ID;
+	long IDProducto;
+	double cantidad;
+	double precio;
+	double iva;
+	double descuento;
+	double subtotal;
+	double total;
+	Producto productoSeleccionado;
+	
+	
+	
+	public Producto getProductoSeleccionado() {
+		return productoSeleccionado;
+	}
+	public void setProductoSeleccionado(Producto productoSeleccionado) {
+		this.productoSeleccionado = productoSeleccionado;
+	}
+	public long getID() {
+		return ID;
+	}
+	public void setID(long iD) {
+		ID = iD;
+	}
+	public long getIDProducto() {
+		return IDProducto;
+	}
+	public void setIDProducto(long iDProducto) {
+		IDProducto = iDProducto;
+	}
+	public double getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(double cantidad) {
+		this.cantidad = cantidad;
+	}
+	public double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	public double getIva() {
+		return iva;
+	}
+	public void setIva(double iva) {
+		this.iva = iva;
+	}
+	public double getDescuento() {
+		return descuento;
+	}
+	public void setDescuento(double descuento) {
+		this.descuento = descuento;
+	}
+	public double getSubtotal() {
+		return subtotal;
+	}
+	public void setSubtotal(double subtotal) {
+		this.subtotal = subtotal;
+	}
+	public double getTotal() {
+		return total;
+	}
+	public void setTotal(double total) {
+		this.total = total;
+	}
+}
